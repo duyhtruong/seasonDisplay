0b3fb1f54a87f093a98405e36a764b075ddff845
Duy Truong <duytruongh@gmail.com>
2018-12-20 15:19:36 -0800

    Updates

---
+++ precache-manifest.0c899a9b63c7cf13dc244e1df87b1064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50a122d58f22c89b43e",
+    "url": "/duyhtruong/seasonDisplay/static/js/runtime~main.d50a122d.js"
+  },
+  {
+    "revision": "f40b8ab47cf384847c83",
+    "url": "/duyhtruong/seasonDisplay/static/js/main.f40b8ab4.chunk.js"
+  },
+  {
+    "revision": "619bf66c065dd1aac483",
+    "url": "/duyhtruong/seasonDisplay/static/js/1.619bf66c.chunk.js"
+  },
+  {
+    "revision": "f40b8ab47cf384847c83",
+    "url": "/duyhtruong/seasonDisplay/static/css/main.faad352c.chunk.css"
+  },
+  {
+    "revision": "612c5750c2fcc1b80e6f146c1a621709",
+    "url": "/duyhtruong/seasonDisplay/index.html"
+  }
+];
